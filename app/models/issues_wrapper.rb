bf74494535ecca9a6d3eeb3a07b237dcdc72f006
Fredirk August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2016-10-19 04:10:32 -0400

    move to model

---
+++ app/models/issues_wrapper.rb
@@ -0,0 +1,20 @@
+class IssuesWrapper
+  class << self
+    ISSUE_PATH = 'https://api.github.com/repos/mikaelbr/open-source-ideas/issues'.freeze
+
+    def get_issues(labels = [])
+      all_issues_json = JSON.parse(Net::HTTP.get(URI(ISSUE_PATH)))
+
+      filtered_issues = []
+
+      all_issues_json.each do |i|
+        if i['labels'].any? && labels.any?
+          i_labels = i['labels'].map { |l| l['name'] }
+          filtered_issues << i if (i_labels - labels).empty?
+        else
+          filtered_issues << i
+        end
+      end
+    end
+  end
+end
